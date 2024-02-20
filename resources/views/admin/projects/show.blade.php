aefd3e4ff1a876522f50ee75501d2eaa62dbf1b5
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-20 19:05:50 +0100

    Modified view of show with type of every project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,6 +10,13 @@
                 <img class="card-img-top" src="{{$project->proj_image}}" alt="{{$project->title}} image cap">
                 <div class="card-body">
                   <h5 class="card-title">{{$project->title}}</h5>
+                  <p class="card-text">
+                    Type of project: {{isset($project->type_id) ? '' : 'Nessuno'  }}
+                    @if ($project->type_id === 1) Back-end
+                    @elseif ($project->type_id === 2) Front-end 
+                    @elseif ($project->type_id === 3) Full-stack 
+                    @endif                  
+                  </p>
                   <p class="card-text">{{$project->description}}</p>
                   <p class="card-text">This project was made in: {{$project->no_days_taken}} days</p>
                   <p class="card-text">Start date: {{$project->start_date}}</p>
