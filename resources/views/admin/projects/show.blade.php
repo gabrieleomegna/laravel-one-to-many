14a76fce513e6480f81f561e8036eab09d598bd5
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-20 19:50:02 +0100

    modified view of show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -11,11 +11,11 @@
                 <div class="card-body">
                   <h5 class="card-title">{{$project->title}}</h5>
                   <p class="card-text">
-                    Type of project: {{isset($project->type_id) ? '' : 'Nessuno'  }}
-                    @if ($project->type_id === 1) Back-end
+                    Type of project: {{isset($project->type_id) ? $project->type->type_name : 'Nessuno'  }} 
+                    {{--@if ($project->type_id === 1) Back-end
                     @elseif ($project->type_id === 2) Front-end 
                     @elseif ($project->type_id === 3) Full-stack 
-                    @endif                  
+                    @endif--}}          
                   </p>
                   <p class="card-text">{{$project->description}}</p>
                   <p class="card-text">This project was made in: {{$project->no_days_taken}} days</p>
